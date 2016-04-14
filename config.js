1e54caee576ffe724daaaa60432f9c2d3da0817f
Vinoth <vipatcts@gmail.com>
2016-04-14 12:46:35 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
 	twitter: {
-  	consumer_key: '63ZjXvSKRxGCzTSzH63DgEv0Z',
-		consumer_secret: 'OPpQkeQ7z4DzTYc204Kun0kjKys2pyV2Oyp7BexRbrNuxrvbiZ',
-		access_token: '17917729-Sg2OeUAv56AVimtXHIkYvUe9EBfwQnFxo6h5SXRHU',
-		access_token_secret: 'XP2juHvVDu7tNvzKh8wqPXX9jibLC0lJkcoAWg5G9bTzx'
+  	consumer_key: '*****************',
+		consumer_secret: '*****',
+		access_token: '**********',
+		access_token_secret: '***********'
 	},
 	kafka: {
 		host:'localhost',
